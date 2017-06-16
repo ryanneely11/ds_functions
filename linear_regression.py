1055fc47a20903758a79526769f231a4774afb61
Ryan Neely <ryanneely11@gmail.com>
2017-06-16 11:50:33 -0700

    Linear regression now functions for multi-neuron multi-timestep data

--- linear_regression.py
+++ linear_regression.py
@@ -6,6 +6,7 @@ import numpy as np
 import model_fitting as mf
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
+import multiprocessing as mp
 
 
 """
@@ -40,70 +41,147 @@ def get_regressors(trial_data):
 	return regressors
 
 """
-A function to fit a cross-validated logistic regressions, and return the 
-model accuracy using three-fold cross-validation.
+A function to fit an OLS linear regression model, and return the significance of the 
+coefficients.
 inputs:
-	X: the independent data; could be spike rates over period.
-		in shape samples x features (ie, trials x spike rates)
-	y: the class data; should be binary. In shape (trials,)
-	n_iter: the number of times to repeat the x-validation (mean is returned)
+	X: the regressor data, should be n-observations x k-regressors
+	y: the spike rate for each trial in a given bin. In shape (trials,)
 Returns:
-	accuracy: mean proportion of test data correctly predicted by the model.
-	llr_p: The chi-squared probability of getting a log-likelihood ratio statistic greater than llr.
-		 llr has a chi-squared distribution with degrees of freedom df_model
+	p-values: the significance of each of the regressor coefficients
 """
-def lin_fit(X,y,n_iter=5,add_constant=True):
+def lin_fit(X,y,add_constant=True):
 	##get X in the correct shape for sklearn function
 	if len(X.shape) == 1:
 		X = X.reshape(-1,1)
 	if add_constant:
-		y = sm.add_constant(y)
-	accuracy = np.zeros(n_iter)
-	for i in range(n_iter):
-		##split the data into train and test sets
-		X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33,random_state=42)
-		##now fit to the test data
-		model = sm.OLS(y_train,X_train,hasconst=True)
-		results = model.fit(method='pinv')
-		"""
-		Some accuracy testing here
-		"""
+		X = sm.add_constant(X)
+	##uncomment later to do some cross-validated accuracy testing
+	# accuracy = np.zeros(n_iter)
+	# for i in range(n_iter):
+	# 	##split the data into train and test sets
+	# 	X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33,random_state=42)
+	# 	##now fit to the test data
+	# 	model = sm.OLS(y_train,X_train,hasconst=True)
+	# 	results = model.fit(method='pinv')
+	# 	"""
+	# 	Some accuracy testing here
+	# 	"""
 	##now get the p-value info for this model
 	model = sm.OLS(y,X,hasconst=True)
 	results = model.fit(method='pinv')
-	return results
+	return results.pvalues[1:]
 
 """
 A function to perform a permutation test for significance
-by shuffling the training data. Uses the cross-validation strategy above.
+by shuffling the training data.
 Inputs:
 	args: a tuple of arguments, in the following order:
 		X: the independent data; trials x features
 		y: the class data, in shape (trials,)
-		n_iter_cv: number of times to run cv on each interation of the test
 		n_iter_p: number of times to run the permutation test
 returns:
-	accuracy: the computed accuracy of the model fit
 	p_val: proportion of times that the shuffled accuracy outperformed
 		the actual data (significance test)
 """
-def permutation_test(args):
+def permutation_test(X,y,n_iter=1000,add_constant=True):
 	##parse the arguments tuple
-	X = args[0]
-	y = args[1]
-	n_iter_cv = args[2]
-	n_iter_p = args[3]
-	##get the accuracy of the real data, to use as the comparison value
-	a_actual,llr_p = log_fit(X,y,n_iter=n_iter_cv)
+	if add_constant:
+		X = sm.add_constant(X)
+	##get the coefficients of the real data, to use as the comparison value
+	model = sm.OLS(y,X,has_constant=True)
+	results = model.fit(method='pinv')
+	coeffs = results.params[1:]##first index is the constant
 	#now run the permutation test, keeping track of how many times the shuffled
 	##accuracy outperforms the actual
-	times_exceeded = 0
-	chance_rates = [] 
-	for i in range(n_iter_p):
+	times_exceeded = np.zeros(X.shape[1]-1)
+	for i in range(n_iter):
 		y_shuff = np.random.permutation(y)
-		a_shuff,llr_p_shuff = log_fit(X,y_shuff,n_iter=n_iter_cv)
-		if a_shuff > a_actual:
-			times_exceeded += 1
-		chance_rates.append(a_shuff)
-	return a_actual, np.asarray(chance_rates).mean(), float(times_exceeded)/n_iter_p, llr_p
-	
+		model = sm.OLS(y_shuff,X,has_constant=True)
+		results = model.fit(method='pinv')
+		coeffs_shuff = results.params[1:]
+		times_exceeded += (np.abs(coeffs_shuff)>np.abs(coeffs)).astype(int)
+	return times_exceeded/n_iter
+
+"""
+A function to analyze the significance of a regressor/regressand pair over
+the course of several timesteps. It is expected that the regressor (X) values
+are constant while the y-values are changing. Ie, the spike rates (y) over the
+course of trials (X) with fixed regressor values.
+Inputs: (in list format)
+	X: regressor array, size n-observations x k-features
+	Y: regressand array, size n-observations x t bins/timesteps
+	add_constant: if True, adds a constant
+	n_iter: number of iterations to run the permutation test. if 0, no 
+		permutation test is performed.
+Returns:
+	f_pvals: pvalues of each coefficient at each time step using f-test statistic (coeffs x bins)
+	p_pvals: pvals using permutation test
+"""
+def regress_timecourse(args):
+	##parse args
+	X = args[0]
+	y = args[1]
+	add_constant = args[2]
+	n_iter = args[3]
+	if add_constant:
+		n_coeffs = X.shape[1]
+	else:
+		n_coeffs = X.shape[1]-1
+	n_bins = y.shape[1] ##number of time bins
+	##setup output
+	f_pvals = np.zeros((n_coeffs,n_bins))
+	p_pvals = np.zeros((n_coeffs,n_bins))
+	##run through analysis at each time step
+	for b in range(n_bins):
+		f_pvals[:,b] = lin_fit(X,y[:,b],add_constant=add_constant)
+		if n_iter > 0:
+			p_pvals[:,b] = permutation_test(X,y[:,b],add_constant=add_constant,n_iter=n_iter)
+		else:
+			p_pvals[:,b] = np.nan
+	return f_pvals,p_pvals
+
+"""
+a function to do regression on a spike matrix consisting of
+binned spike data from many neurons across time. Result is a matrix
+counting the number of significant neurons for each regressor in
+each time bin.
+Inputs:
+	X: regressor array, size n-observations x k-features
+	Y: regressand array, size n-observations x p neurons x t bins/timesteps
+	add_constant: if True, adds a constant
+	n_iter: number of iterations to run the permutation test. if 0, no 
+		permutation test is performed.
+Returns:
+	f_counts: number of significant neurons at each time point according to f-test
+	p_counts: same thing but using a permutation test
+"""
+def regress_spike_matrix(X,Y,add_constant=True,n_iter=1000):
+	n_neurons = Y.shape[1]
+	if add_constant:
+		n_coeffs = X.shape[1]
+	else:
+		n_coeffs = X.shape[1]-1
+	n_bins = Y.shape[2] ##number of time bins
+	##setup output data
+	f_pvals = np.zeros((n_neurons,n_coeffs,n_bins))
+	p_pvals = np.zeros((n_neurons,n_coeffs,n_bins))
+	##basically just perform regress_timecourse for each neuron
+	##use multiprocessing to speed up the permutation testing.
+	arglist = [[X,Y[:,n,:],add_constant,n_iter] for n in range(n_neurons)]
+	pool = mp.Pool(processes=mp.cpu_count())
+	async_result = pool.map_async(regress_timecourse,arglist)
+	pool.close()
+	pool.join()
+	results = async_result.get()
+	for n in range(len(results)):
+		f_pvals[n,:,:] = results[n][0]
+		p_pvals[n,:,:] = results[n][1]
+	##now we want to count up the significant neurons
+	p_thresh = 0.05
+	f_counts = (f_pvals <= p_thresh).sum(axis=0)
+	p_counts = (p_pvals <= p_thresh).sum(axis=0)
+	return f_counts,p_counts
+
+
+
+
